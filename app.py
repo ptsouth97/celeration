5342d34ad9a0487a276fabcdaade16d0f29f0465
ptsouth97 <ptsouth97@gmail.com>
2020-03-21 19:58:39 -0400

    updated regression model

--- app.py
+++ app.py
@@ -6,44 +6,55 @@ from matplotlib.ticker import MultipleLocator, ScalarFormatter
 import numpy as np
 import datetime
 import os
+from sklearn.linear_model import LinearRegression
 
 
 def main():
 	''' main function'''
 
 	# define region
-	region = 'Italy'
+	countries = ['Italy']
 
-	# define data type  (Confirmed, Deaths, Recovered)
-	data_types = ['Confirmed', 'Deaths']
-
-	# initialize blank DataFrame
-	df = pd.DataFrame()
-
-	for data in data_types:
-
-		# load data
-		confirmed = load_data(data)
+	# get a list of the countries
+	'''countries = load_data('Confirmed')
+	countries = countries['Country/Region']
+	countries = countries.drop_duplicates()
+	countries = countries.tolist()'''
 	
-		# slice regional data only
-		region_df = get_data(confirmed, region)
+	for country in countries:
+
+		print('Now processing {}...'.format(country))
+
+		# define data type  (Confirmed, Deaths, Recovered)
+		data_types = ['Confirmed', 'Deaths']
+
+		# initialize blank DataFrame
+		df = pd.DataFrame()
+
+		for data in data_types:
+
+			# load data
+			confirmed = load_data(data)
 	
-		# sum cumulative values for each day
-		cumulative = count_data(region_df)
+			# slice regional data only
+			region_df = get_data(confirmed, country)
+	
+			# sum cumulative values for each day
+			cumulative = count_data(region_df)
 
-		# calculate daily counts
-		daily = calc_daily_count(cumulative)
+			# calculate daily counts
+			daily = calc_daily_count(cumulative)
 
-		# combine 2 series
-		addition = pd.concat([cumulative, daily], axis=1)
+			# combine 2 series
+			addition = pd.concat([cumulative, daily], axis=1)
 
-		# add to dataframe
-		df = pd.concat([df, addition], axis=1)
+			# add to dataframe
+			df = pd.concat([df, addition], axis=1)
 
-	df.columns=(['Cumulative cases', 'Daily cases', 'Cumulative deaths', 'Daily deaths'])
+		df.columns=(['Cumulative cases', 'Daily cases', 'Cumulative deaths', 'Daily deaths'])
 
-	# plot data
-	plot_data(df, region)
+		# plot data
+		plot_data(df, country)
 
 
 def load_data(data):
@@ -97,19 +108,42 @@ def calc_daily_count(df):
 	return df
 
 
-def plot_data(df, area):
+def regression(df):
+	''' performs curve fit'''
+
+	df = df.replace(to_replace=0, value=1)
+	start_date = df.index[0]
+	
+	num = len(df)
+	X = np.arange(1, num+1).reshape(-1, 1)
+	Y = df.iloc[:, 1].values.reshape(-1, 1)
+	
+	linear_regressor = LinearRegression()
+	linear_regressor.fit(X, np.log(Y))
+	Y_pred = linear_regressor.predict(X)
+
+	predictions = pd.DataFrame(Y_pred, index=df.index)
+	predictions = predictions.apply(np.exp, axis=0)
+
+	return predictions
+
+
+def state_data(df, area):
 	''' plots data'''
 
-	#is_sc = df['Province/State']=='South Carolina'
-	#df = df[is_sc]
-	#df.reset_index(drop=True, inplace=True)
-	#df.drop(['Province/State', 'Country/Region'], axis=1, inplace=True)
-	#df = df.T
-	#print(df.iloc[0])
-	#df.set_index(df.iloc[0], inplace=True)
+	is_sc = df['Province/State']=='South Carolina'
+	df = df[is_sc]
+	df.reset_index(drop=True, inplace=True)
+	df.drop(['Province/State', 'Country/Region'], axis=1, inplace=True)
+	df = df.T
+	print(df.iloc[0])
+	df.set_index(df.iloc[0], inplace=True)
 
-	# Combine 2 series into dataframe
-	# df = pd.concat([s1, s2], axis=1)
+	return
+
+
+def plot_data(df, area):
+	''' plots data'''
 
 	# get the current date
 	date = df.index[-1]
@@ -117,20 +151,26 @@ def plot_data(df, area):
 	# replace date slashes with dashes
 	date = date.replace('/', '-')
 
+	# replace NaN values with 0
+	df = df.fillna(0)
+
 	# change index to datetime
 	df.index = pd.to_datetime(df.index, infer_datetime_format=True) #format='%m/%-d/%y')
-	print(df)
-
 	
-	# change series back to df
-	#df = pd.DataFrame(df)
+	Y_pred = regression(df)
 
-	# assign column name
-	#df.columns = ['Cumulative', 'Daily']
+	# Add predictions to dataframe
+	df = pd.concat([df, Y_pred], axis=1)
+	df = df.rename(columns={0: 'model'})
+	print(df)	
 
-	# plot df
-	ax = df.plot(marker='o', markersize=3, linewidth=1, logy=True, legend=True)
-
+	#ax = plt.plot(df['Daily cases'])
+	ax = df['Cumulative cases'].plot(kind='line', marker='.', linewidth=1, logy=True, legend=True)
+	ax = df['Daily cases'].plot(kind='line', marker='.', linewidth=1, logy=True, legend=True)
+	ax = df['Cumulative deaths'].plot(kind='line', marker='.', linewidth=1, logy=True, legend=True)
+	ax = df['Daily deaths'].plot(kind='line', marker='.', linewidth=1, logy=True, legend=True)
+	ax = df['model'].plot(kind='line', marker=None, linewidth=1, logy=True, legend=True)
+	
 	# set the range for the y axis
 	ax.set_ylim([1, 1000000])
 
@@ -138,9 +178,6 @@ def plot_data(df, area):
 	ax.yaxis.grid(True, which='minor', linestyle='-', alpha=0.5)
 	ax.yaxis.grid(True, which='major', linestyle='-', alpha=0.8)
 	
-	# move x-axis labels to the top
-	# ax.xaxis.set_ticks_position('top')
-
 	# set the range for the x axis
 	ax.set_xlim([datetime.date(2019, 12,29), datetime.date(2020, 5, 17)])
 
@@ -172,7 +209,7 @@ def plot_data(df, area):
 	plt.savefig(area + '.png')
 	
 	# change back to original working directory
-	os.chdir('..')
+	os.chdir('../..')
 
 	# display the chart
 	plt.show()
